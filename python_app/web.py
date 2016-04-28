fe200ddd9db718e3ca843e4e6e7e78907e3fd54d
Leonardo da Mata <barroca@gmail.com>
2016-04-28 11:39:00 -0300

    add htaccess and ping url

--- python_app/web.py
+++ python_app/web.py
@@ -2,15 +2,21 @@
 import socket
 
 from flask import Flask, jsonify
+from datetime import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def home():
     local_ip = socket.gethostbyname(socket.gethostname())
+    print "a"
 
     return jsonify(ip=local_ip)
+@application.route("/ping")
+def ping():
+    return jsonify(time=datetime.now().isoformat())
+
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
 
